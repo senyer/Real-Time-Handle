66153b1aafe331996f32b6462587b9706db6ef05
senyer <576085903@qq.com>
2020-02-20 14:54:30 +0800

    Fix Readme

--- src/main/java/com/sssj/webservice/WebServiceHandler.java
+++ src/main/java/com/sssj/webservice/WebServiceHandler.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
